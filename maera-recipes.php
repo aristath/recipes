7f0d0c7e3087d5ef7bfdb17e774469c8783574fd
Aristeides Stathopoulos <aristath@gmail.com>
2015-05-16 20:54:41 +0300

    add ingredients taxonomy

--- maera-recipes.php
+++ maera-recipes.php
@@ -53,3 +53,41 @@
 
  // Hook into the 'init' action
  add_action( 'init', 'maera_recipes_recipe_post_type', 0 );
+
+ // Register Custom Taxonomy
+ function maera_recipes_ingredients_taxonomy() {
+
+ 	$labels = array(
+ 		'name'                       => _x( 'Ingredients', 'Taxonomy General Name', 'maera-recipes' ),
+ 		'singular_name'              => _x( 'Ingredient', 'Taxonomy Singular Name', 'maera-recipes' ),
+ 		'menu_name'                  => __( 'Ingredients', 'maera-recipes' ),
+ 		'all_items'                  => __( 'All Ingredients', 'maera-recipes' ),
+ 		'parent_item'                => __( 'Parent Item', 'maera-recipes' ),
+ 		'parent_item_colon'          => __( 'Parent Item:', 'maera-recipes' ),
+ 		'new_item_name'              => __( 'New Item Name', 'maera-recipes' ),
+ 		'add_new_item'               => __( 'Add New Ingredient', 'maera-recipes' ),
+ 		'edit_item'                  => __( 'Edit Ingredient', 'maera-recipes' ),
+ 		'update_item'                => __( 'Update Ingredient', 'maera-recipes' ),
+ 		'view_item'                  => __( 'View Ingredient', 'maera-recipes' ),
+ 		'separate_items_with_commas' => __( 'Separate ingredients with commas', 'maera-recipes' ),
+ 		'add_or_remove_items'        => __( 'Add or remove ingredients', 'maera-recipes' ),
+ 		'choose_from_most_used'      => __( 'Choose from the most used ingredients', 'maera-recipes' ),
+ 		'popular_items'              => __( 'Popular Ingredients', 'maera-recipes' ),
+ 		'search_items'               => __( 'Search Ingredients', 'maera-recipes' ),
+ 		'not_found'                  => __( 'Not Found', 'maera-recipes' ),
+ 	);
+ 	$args = array(
+ 		'labels'                     => $labels,
+ 		'hierarchical'               => false,
+ 		'public'                     => true,
+ 		'show_ui'                    => true,
+ 		'show_admin_column'          => true,
+ 		'show_in_nav_menus'          => true,
+ 		'show_tagcloud'              => true,
+ 	);
+ 	register_taxonomy( 'ingredient', array( 'recipe' ), $args );
+
+ }
+
+ // Hook into the 'init' action
+ add_action( 'init', 'maera_recipes_ingredients_taxonomy', 0 );
