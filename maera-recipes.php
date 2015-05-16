bac3d07680c4610cf47a8a86f368835718c7b04d
Aristeides Stathopoulos <aristath@gmail.com>
2015-05-16 20:49:38 +0300

    add main plugin file

---
+++ maera-recipes.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name:       Maera Recipes
+ * Plugin URI:        https://press.codes
+ * Version:           0.1.0
+ * Text Domain:       maera-recepes
+ * Domain Path:       /languages/
+ */
