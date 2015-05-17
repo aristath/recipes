29015f055eafc4bc6be2238d6b57cd2f5a804ae4
Aristeides Stathopoulos <aristath@gmail.com>
2015-05-17 03:10:40 +0300

    Add CSS

--- maera-recipes.php
+++ maera-recipes.php
@@ -8,10 +8,12 @@
  */
 
  define( 'MAERA_RECIPES_PATH', plugin_dir_path( __FILE__ ) );
+ define( 'MAERA_RECIPES_URL',  plugins_url( '', __FILE__ ) );
 
  require_once( MAERA_RECIPES_PATH . 'includes/post-type.php' );
  require_once( MAERA_RECIPES_PATH . 'includes/taxonomies.php' );
  require_once( MAERA_RECIPES_PATH . 'includes/fields-acf.php' );
  require_once( MAERA_RECIPES_PATH . 'includes/class-maera-recipes-template.php' );
+ require_once( MAERA_RECIPES_PATH . 'includes/scripts.php' );
 
  $single_recipe_template = new Maera_Recipes_Template();
