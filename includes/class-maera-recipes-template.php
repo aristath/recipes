3a48d69155b1cbf81bf2fff704333367e765b842
Aristeides Stathopoulos <aristath@gmail.com>
2015-05-17 04:44:36 +0300

    implement unit conversions

--- includes/class-maera-recipes-template.php
+++ includes/class-maera-recipes-template.php
@@ -18,7 +18,7 @@ class Maera_Recipes_Template {
         $template  = '<div class="recipe-wrapper">';
         $template .= $this->the_info();
         $template .= '<div class="recipe-flex-wrapper">';
-        $template .= '<div class="ingredients-wrapper">' . $this->the_ingredients() . '</div>';
+        $template .= '<div class="ingredients-wrapper">' . $this->units_switch() . $this->the_ingredients() . '</div>';
         $template .= '<div class="execution">' . $content . '</div>';
         $template .= '</div>';
         $template .= '</div>';
@@ -41,7 +41,7 @@ class Maera_Recipes_Template {
                 $ingredient      = get_term_by( 'id', get_sub_field( 'ingredient' ), 'ingredient' );
 
                 $ingredients .= '<li>';
-                $ingredients .= '<span class="quantity">' . $quantity . '<span class="unit">' . $unit . '</span></span>';
+                $ingredients .= '<span class="quantity">' . $this->unit_conversion( $quantity, $unit, 'value' ) . '<span class="unit">' . $this->unit_conversion( $quantity, $unit, 'unit' ) . '</span></span>';
                 $ingredients .= '<a href="' . get_term_link( $ingredient ) . '">' . $ingredient->name . '</a>';
                 $ingredients .= '</li>';
 
@@ -52,6 +52,193 @@ class Maera_Recipes_Template {
         }
 
         return apply_filters( 'maera_recipes/template/ingredients', $ingredients );
+
+    }
+
+    /**
+     * @var string  numeric or fraction value
+     * @var string  the unit we're using
+     * @var string  what we want to output. can be value/unit
+     */
+    public function unit_conversion( $value, $unit, $output ) {
+        if ( is_singular( 'recipe' ) && isset( $_GET['mode'] ) ) {
+            $mode = $_GET['mode'];
+
+            /**
+             * Do not proceed if this is not a valid mode.
+             * Instead just return the default values.
+             */
+            if ( ! in_array( $mode, array( 'metric', 'imperial', 'us' ) ) ) {
+                return ( 'value' == $output ) ? $value : $unit;
+            }
+
+            // convert fractions to actual numbers
+            switch ( $value ) {
+                case '1/2' :
+                    $value = 0.5; break;
+                case '1/3' :
+                    $value = 0.33; break;
+                case '2/3' :
+                    $value = 0.66; break;
+                case '1/4' :
+                    $value = 0.25; break;
+                case '3/4' :
+                    $value = 0.75; break;
+                default:
+                    $value = (int) $value;
+            }
+
+            // Metric mode
+            if ( 'metric' == $mode ) {
+
+                switch ( $unit ) {
+                    case 'us-gal' :
+                        $value = round( $value * 3785.41 );
+                        $unit  = 'ml';
+                        break;
+                    case 'us-quart' :
+                        $value = round( $value * 946.353 );
+                        $unit  = 'ml';
+                        break;
+                    case 'us-pint' :
+                        $value = round( $value * 473.176 );
+                        $unit  = 'ml';
+                        break;
+                    case 'us-floz' :
+                        $value = round( $value * 29.5735 );
+                        $unit  = 'ml';
+                        break;
+                    case 'imp-gal' :
+                        $value = round( $value * 4546.09 );
+                        $unit  = 'ml';
+                        break;
+                    case 'imp-quart' :
+                        $value = round( $value * 1136.52 );
+                        $unit  = 'ml';
+                        break;
+                    case 'imp-pint' :
+                        $value = round( $value * 568.261 );
+                        $unit  = 'ml';
+                        break;
+                    case 'imp-floz' :
+                        $value = round( $value * 28.4131 );
+                        $unit  = 'ml';
+                        break;
+                    case 'pound' :
+                        $value = round( $value * 453.592 );
+                        $unit  = 'gram';
+                        break;
+                    case 'ounce' :
+                        $value = round( $value * 28.3495 );
+                        $unit  = 'gram';
+                        break;
+                    case 'inch' :
+                        $value = round( $value * 2.54, 1 );
+                        $unit  = 'cm';
+                        break;
+
+                }
+
+            // US mode
+            } elseif ( 'us' == $mode ) {
+
+                switch ( $unit ) {
+
+                    case 'imp-gal' :
+                        $value = round( $value * 1.20095, 2 );
+                        $unit  = 'us-gal';
+                        break;
+                    case 'imp-quart' :
+                        $value = round( $value * 1.20095, 2 );
+                        $unit  = 'us-quart';
+                        break;
+                    case 'imp-pint' :
+                        $value = round( $value * 1.20095, 2 );
+                        $unit = 'us-pint';
+                        break;
+                    case 'imp-oz' :
+                        $value = round( $value * 0.96076, 2 );
+                        $unit  = 'us-oz';
+                        break;
+                    case 'ml' :
+                        $value = round( $value * 0.033814, 2 );
+                        $unit  = 'us-oz';
+                        break;
+                    case 'lt' :
+                        $value = round( $value * 1.05669, 2 );
+                        $unit  = 'us-quart';
+                        break;
+                    case 'gram' :
+                        $value = round( $value * 0.035274, 2 );
+                        $unit  = 'ounce';
+                        break;
+                    case 'kg' :
+                        $value = round( $value * 2.20462, 2 );
+                        $unit  = 'pound';
+                        break;
+                    case 'cm' :
+                        $value = round( $value / 2.54 , 2 );
+                        $unit  = 'inch';
+                        break;
+                    case 'm' :
+                        $value = round( $value * 100 / 2.54 , 2 );
+                        $unit  = 'inch';
+                        break;
+
+                }
+
+            // Imperial mode
+            } elseif ( 'imperial' == $mode ) {
+
+                switch ( $unit ) {
+
+                    case 'us-gal' :
+                        $value = round( $value * 0.832674, 2 );
+                        $unit  = 'imp-gal';
+                        break;
+                    case 'us-quart' :
+                        $value = round( $value * 0.832674, 2 );
+                        $unit  = 'imp-quart';
+                        break;
+                    case 'us-pint' :
+                        $value = round( $value * 0.832674, 2 );
+                        $unit  = 'imp-pint';
+                        break;
+                    case 'us-oz' :
+                        $value = round( $value * 1.04084, 2 );
+                        $unit  = 'imp-oz';
+                        break;
+                    case 'ml' :
+                        $value = round( $value * 0.0351951, 2 );
+                        $unit  = 'imp-oz';
+                    case 'lt' :
+                        $value = round( $value * 1.75975, 2 );
+                        $unit  = 'imp-pint';
+                        break;
+                    case 'gram' :
+                        $value = round( $value * 0.035274, 2 );
+                        $unit  = 'ounce';
+                        break;
+                    case 'kg' :
+                        $value = round( $value * 2.20462, 2 );
+                        $unit  = 'pound';
+                        break;
+                    case 'cm' :
+                        $value = round( $value / 2.54 , 2 );
+                        $unit  = 'inch';
+                        break;
+                    case 'm' :
+                        $value = round( $value * 100 / 2.54 , 2 );
+                        $unit  = 'inch';
+                        break;
+
+                }
+
+            }
+
+        }
+
+        return ( 'value' == $output ) ? $value : $unit;
 
     }
 
@@ -76,6 +263,19 @@ class Maera_Recipes_Template {
         $template .= '</div>';
 
         return apply_filters( 'maera_recipes/template/info', $template );
+
+    }
+
+    public function units_switch() {
+
+        $mode = isset( $_GET['mode'] ) ? ' ' . $_GET['mode'] : '';
+        $template  = '<div class="units-switch' . $mode . '">';
+        $template .= '<a href="?mode=metric" class="metric">' . __( 'Metric', 'maera-recipes' ) . '</a>';
+        $template .= '<a href="?mode=imperial" class="imperial">' . __( 'Imperial', 'maera-recipes' ) . '</a>';
+        $template .= '<a href="?mode=us" class="us">' . __( 'US', 'maera-recipes' ) . '</a>';
+        $template .= '</div>';
+
+        return $template;
 
     }
 
