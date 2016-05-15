96fbc329b139bbfa61f781a907d6b7683e797383
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-15 15:39:03 +0300

    remove the ingredients taxonomy

--- includes/taxonomies.php
+++ includes/taxonomies.php
@@ -1,40 +1,5 @@
 <?php
 
-// Register Custom Taxonomy
-function recipes_ingredients_taxonomy() {
-
-    $labels = array(
-        'name'                       => _x( 'Ingredients', 'Taxonomy General Name', 'recipes' ),
-        'singular_name'              => _x( 'Ingredient', 'Taxonomy Singular Name', 'recipes' ),
-        'menu_name'                  => __( 'Ingredients', 'recipes' ),
-        'all_items'                  => __( 'All Ingredients', 'recipes' ),
-        'parent_item'                => __( 'Parent Item', 'recipes' ),
-        'parent_item_colon'          => __( 'Parent Item:', 'recipes' ),
-        'new_item_name'              => __( 'New Item Name', 'recipes' ),
-        'add_new_item'               => __( 'Add New Ingredient', 'recipes' ),
-        'edit_item'                  => __( 'Edit Ingredient', 'recipes' ),
-        'update_item'                => __( 'Update Ingredient', 'recipes' ),
-        'view_item'                  => __( 'View Ingredient', 'recipes' ),
-        'separate_items_with_commas' => __( 'Separate ingredients with commas', 'recipes' ),
-        'add_or_remove_items'        => __( 'Add or remove ingredients', 'recipes' ),
-        'choose_from_most_used'      => __( 'Choose from the most used ingredients', 'recipes' ),
-        'popular_items'              => __( 'Popular Ingredients', 'recipes' ),
-        'search_items'               => __( 'Search Ingredients', 'recipes' ),
-        'not_found'                  => __( 'Not Found', 'recipes' ),
-    );
-    $args = array(
-        'labels'                     => $labels,
-        'hierarchical'               => false,
-        'public'                     => true,
-        'show_ui'                    => true,
-        'show_admin_column'          => true,
-        'show_in_nav_menus'          => true,
-        'show_tagcloud'              => true,
-    );
-    register_taxonomy( 'ingredient', array( 'recipe' ), $args );
-
-}
-
 // Hook into the 'init' action
 add_action( 'init', 'recipes_ingredients_taxonomy', 0 );
 
