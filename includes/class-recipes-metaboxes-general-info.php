38a9bf9d3ca6d4360d587b8e802f7e2b98f008da
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-15 14:24:55 +0300

    styling improvements for the general-info metabox.

--- includes/class-recipes-metaboxes-general-info.php
+++ includes/class-recipes-metaboxes-general-info.php
@@ -73,17 +73,19 @@ if ( ! class_exists( 'Recipes_Metaboxes_General_Info' ) ) {
 			$preparation_time = get_post_meta( $post->ID, 'preparation_time', true );
 			$cook_time        = get_post_meta( $post->ID, 'cook_time', true );
 			?>
-			<div class="recipes servings-wrapper">
-				<label for="servings_field"><?php _e( 'Servings', 'recipes' ); ?></label>
-				<input type="number" id="servings_field" name="servings_field" value="<?php echo absint( $servings ); ?>" size="25" />
-			</div>
-			<div class="recipes preparation_time_wrapper">
-				<label for="preparation_time"><?php _e( 'Preparation Time (minutes)', 'recipes' ); ?></label>
-				<input type="number" id="preparation_time" name="preparation_time" value="<?php echo absint( $preparation_time ); ?>" size="25" />
-			</div>
-			<div class="recipes cook_time_wrapper">
-				<label for="cook_time"><?php _e( 'Cook Time (minutes)', 'recipes' ); ?></label>
-				<input type="number" id="cook_time" name="cook_time" value="<?php echo absint( $cook_time ); ?>" size="25" />
+			<div class="recipes general-info fields-wrapper">
+				<div class="recipes servings-wrapper">
+					<label for="servings_field"><?php _e( 'Servings', 'recipes' ); ?></label>
+					<input type="number" id="servings_field" name="servings_field" value="<?php echo absint( $servings ); ?>" size="25" />
+				</div>
+				<div class="recipes preparation_time_wrapper">
+					<label for="preparation_time"><?php _e( 'Preparation Time (minutes)', 'recipes' ); ?></label>
+					<input type="number" id="preparation_time" name="preparation_time" value="<?php echo absint( $preparation_time ); ?>" size="25" />
+				</div>
+				<div class="recipes cook_time_wrapper">
+					<label for="cook_time"><?php _e( 'Cook Time (minutes)', 'recipes' ); ?></label>
+					<input type="number" id="cook_time" name="cook_time" value="<?php echo absint( $cook_time ); ?>" size="25" />
+				</div>
 			</div>
 			<?php
 		}
