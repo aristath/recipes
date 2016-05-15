a8d7c85ce03815d9cda53d310f3031cc75031b38
Aristeides Stathopoulos <aristath@gmail.com>
2016-05-15 14:01:25 +0300

    spacing & minor bugfix.

--- includes/class-recipes-metaboxes-ingredients.php
+++ includes/class-recipes-metaboxes-ingredients.php
@@ -18,56 +18,59 @@ if ( ! class_exists( 'Recipes_Metaboxes_Ingredients' ) ) {
 			);
 			parent::__construct();
 		}
-	    /**
-	     * Save the meta when the post is saved.
+		/**
+		 * Save the meta when the post is saved.
 		 *
 		 * @access public
-	     * @param int $post_id The ID of the post being saved.
-	     */
-	    public function save( $post_id ) {
+		 * @param int $post_id The ID of the post being saved.
+		 */
+		public function save( $post_id ) {
 
-	        // Check if our nonce is set.
-	        if ( ! isset( $_POST['recipes_ingredients_nonce'] ) ) {
-	            return $post_id;
-	        }
+			// Check if our nonce is set.
+			if ( ! isset( $_POST['recipes_ingredients_nonce'] ) ) {
+				return $post_id;
+			}
 
-	        // Verify that the nonce is valid.
-	        if ( ! wp_verify_nonce( $_POST['recipes_ingredients_nonce'], 'recipes_inner_custom_box' ) ) {
-	            return $post_id;
-	        }
+			// Verify that the nonce is valid.
+			if ( ! wp_verify_nonce( $_POST['recipes_ingredients_nonce'], 'recipes_inner_custom_box' ) ) {
+				return $post_id;
+			}
 
 			// If this is an autosave, our form has not been submitted,
-	        // so we don't want to do anything.
-	        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-	            return $post_id;
-	        }
+			// so we don't want to do anything.
+			if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+				return $post_id;
+			}
 
-	        // Check the user's permissions.
+			// Check the user's permissions.
 			if ( ! current_user_can( 'edit_post', $post_id ) ) {
 				return $post_id;
-	        }
+			}
 
-	        // Sanitize the user input.
+			// Sanitize the user input.
 			$ingredients = $_POST['ingredients'];
 
-	        // Update the meta field.
+			// Update the meta field.
 			update_post_meta( $post_id, 'ingredients', $ingredients );
-	    }
+		}
 
 
-	    /**
-	     * Render Meta Box content.
-	     *
+		/**
+		 * Render Meta Box content.
+		 *
 		 * @access public
-	     * @param WP_Post $post The post object.
-	     */
-	    public function callback( $post ) {
+		 * @param WP_Post $post The post object.
+		 */
+		public function callback( $post ) {
 
-	        // Add an nonce field so we can check for it later.
-	        wp_nonce_field( 'recipes_inner_custom_box', 'recipes_ingredients_nonce' );
+			// Add an nonce field so we can check for it later.
+			wp_nonce_field( 'recipes_inner_custom_box', 'recipes_ingredients_nonce' );
 
-	        $ingredients = get_post_meta( $post->ID, 'ingredients', true );
-	        ?>
+			$ingredients = get_post_meta( $post->ID, 'ingredients', true );
+			if ( empty( $ingredients ) ) {
+				$ingredients = array( array( 'quantity' => '', 'ingredient' => '' ) );
+			}
+			?>
 			<div class="ingredients-repeater">
 				<div data-repeater-list="ingredients">
 					<?php foreach ( $ingredients as $key => $ingredient ) : ?>
@@ -81,6 +84,6 @@ if ( ! class_exists( 'Recipes_Metaboxes_Ingredients' ) ) {
 				<input data-repeater-create class="button button-primary" type="button" value="Add"/>
 			</div>
 			<?php
-	    }
+		}
 	}
 }
--- includes/class-recipes-metaboxes.php
+++ includes/class-recipes-metaboxes.php
@@ -16,49 +16,49 @@ if ( ! class_exists( 'Recipes_Metaboxes' ) ) {
 		 * Contains all hooks & actions needed.
 		 *
 		 * @access public
-	     */
-	    public function __construct() {
+		 */
+		public function __construct() {
 
 			add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
 			add_action( 'save_post', array( $this, 'save' ) );
 
-	    }
+		}
 
 		/**
 		 * Adds the meta box container.
 		 *
 		 * @access public
 		 * @param string $post_type Thje post type.
-	     */
-	    public function add_meta_box( $post_type ) {
+		 */
+		public function add_meta_box( $post_type ) {
 
-	        if ( 'recipe' === $post_type ) {
+			if ( 'recipe' === $post_type ) {
 				add_meta_box(
 					$this->metabox_args['id'],
 					$this->metabox_args['title'],
 					array( $this, 'callback' ),
-	                $post_type,
+					$post_type,
 					$this->metabox_args['context'],
 					$this->metabox_args['priority']
-	            );
-	        }
-	    }
+				);
+			}
+		}
 
-	    /**
-	     * Save the meta when the post is saved.
+		/**
+		 * Save the meta when the post is saved.
 		 *
 		 * @access public
-	     * @param int $post_id The ID of the post being saved.
-	     */
-	    public function save( $post_id ) {}
+		 * @param int $post_id The ID of the post being saved.
+		 */
+		public function save( $post_id ) {}
 
 
-	    /**
-	     * Render Meta Box content.
-	     *
+		/**
+		 * Render Meta Box content.
+		 *
 		 * @access public
-	     * @param WP_Post $post The post object.
-	     */
-	    public function callback( $post ) {}
+		 * @param WP_Post $post The post object.
+		 */
+		public function callback( $post ) {}
 	}
 }
