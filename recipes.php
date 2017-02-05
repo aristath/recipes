cffdd2c488750dcfad6c26d778eb786deca1f15d
Aristeides Stathopoulos <aristath@gmail.com>
2017-02-05 09:39:01 +0200

    Add dependencies for scripts

--- recipes.php
+++ recipes.php
@@ -135,7 +135,7 @@ class Recipes {
 	 */
 	public function admin_enqueue_scripts() {
 
-		wp_enqueue_script( 'recipes', trailingslashit( self::$plugin_url ) . 'assets/js/recipes-admin.js', array( 'jquery' ) );
+		wp_enqueue_script( 'recipes', trailingslashit( self::$plugin_url ) . 'assets/js/recipes-admin.js', array( 'jquery', 'underscore', 'wp-util' ) );
 		wp_enqueue_style( 'recipes-admin', self::$plugin_url . '/assets/css/admin-post-edit.css' );
 
 	}
